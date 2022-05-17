9200276f54935d1f4569b636b5a8e377bb9e6415
Kate Koltsova <kate.petronyuk@gmail.com>
2022-05-17 22:43:45 +0300

    HOMEWORK 12: Added PHPDoc for routing classes, install phpDocumentor and generation documents

--- src/Router.php
+++ src/Router.php
@@ -4,13 +4,33 @@ namespace Koltsova\Router;
 
 use Aigletter\Contracts\Routing\RouteInterface;
 
+/**
+ * Class router implements the RouteInterface from the Aigletter\Contracts package.
+ * Contains a config file with actions for specific URLs.
+ * Allows you to pass the necessary parameters to the corresponding methods of the called classes.
+ * Parameters are determined using reflection from a GET request.
+ * The router is designed as a composer package according to the psr-4 standard, and posted on packagist.
+ */
 class Router implements RouteInterface
 {
-
+    /**
+     * Contains routing paths and actions for them.
+     * @var array
+     */
     public array $router = [];
 
+    /**
+     * Contains value of necessary parameters for methods using in action.
+     * @var array
+     */
     public array $args = [];
 
+    /**
+     * The main function of the class. Executes a callback of an action predefined in the array $router.
+     * @param string $uri
+     * @return callable
+     * @throws \Exception
+     */
     public function route(string $uri): callable
     {
         $uri = parse_url($uri, PHP_URL_PATH);
@@ -33,6 +53,13 @@ class Router implements RouteInterface
         }
     }
 
+    /**
+     * Adds a pair of path-action values from the config file to the array $router.
+     * @param string $uri
+     * @param $method
+     * @return string
+     * @throws \Exception
+     */
     public function addRoute(string $uri, $method): string
     {
         if (empty($uri) || empty($method)) {
@@ -43,6 +70,17 @@ class Router implements RouteInterface
         }
     }
 
+    /**
+     * Gets an array of required parameters for the method called in the URL-action.
+     * Gets the values of these parameters from the GE-request (if they exist)
+     * or uses the default values (for optional parameters)
+     * and fills in the array $args.
+     * @param $className
+     * @param $methodName
+     * @return void
+     * @throws \ReflectionException
+     * @throws \Exception
+     */
     public function getMethodParameters($className, $methodName)
     {
         $reflection = new \ReflectionMethod($className, $methodName);
--- src/RouterFactory.php
+++ src/RouterFactory.php
@@ -4,10 +4,24 @@ namespace Koltsova\Router;
 
 use Aigletter\Contracts\ComponentFactory;
 
+/**
+ * Class Router factory extends ComponentFactory from the Aigletter\Contracts package.
+ * Using for creating object of Router class and writing path-action from config file with addRoute function of Router.
+ */
 class RouterFactory extends ComponentFactory
 {
+    /**
+     * Consist link to Router object.
+     * @var Router
+     */
     protected static $router;
 
+    /**
+     * The main function of class.
+     * Creating object of Router class and writing path-action from config file with addRoute function of Router.
+     * @return Router
+     * @throws \Exception
+     */
     protected function createConcreteComponent()
     {
         if (self::$router === null) {
